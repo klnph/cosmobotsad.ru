0c3eeaf315dd9c6d1f9b0ac1dda9db8496cce9f3
Viacheslav Adishchev <adischev@gmail.com>
2025-05-20 18:13:11 +0300

    Engueue parent styles to the child theme

--- wp-content/themes/cosmobotsad/functions.php
+++ wp-content/themes/cosmobotsad/functions.php
@@ -1,8 +1,12 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', function() {
+if (! defined('WP_DEBUG')) {
+	die( 'Direct access forbidden.' );
+}
+add_action( 'wp_enqueue_scripts', function () {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'chichi-style',  get_stylesheet_uri() );
-} );
+});
 
 add_action('wp_head', function() {
 	?>
